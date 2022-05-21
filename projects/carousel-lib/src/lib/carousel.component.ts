d4c73d0390a081f462c844990a8a15799a81982d
Brandon Wood <brandon.wood@mtn.com>
2022-05-21 18:26:43 +0200

    initial commit for projects

---
+++ projects/carousel-lib/src/lib/carousel.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mtn-carousel',
+  template: `
+    <p>
+      carousel-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/carousel-lib/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of carousel-lib
+ */
+
+export * from './lib/carousel.service';
+export * from './lib/carousel.component';
+export * from './lib/carousel.module';
