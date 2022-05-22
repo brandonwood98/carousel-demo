20153d8f00d580ef6a8ebfaf71445726585ca300
Brandon Wood <brandon.wood@mtn.com>
2022-05-22 13:03:36 +0200

    add toggle for dark mode

--- projects/carousel-demo/src/app/app.module.ts
+++ projects/carousel-demo/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {CarouselModule} from "../../../carousel-lib/src/lib/carousel.module";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import {CarouselModule} from "../../../carousel-lib/src/lib/carousel.module";
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     CarouselModule
   ],
   providers: [],
